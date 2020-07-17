e581fcabb9c53e0815d8a1c30b9809555c1afdb1
yiiu <yu.gintoki@gmail.com>
2020-07-17 16:24:00 +0800

    chore: update enum

--- enum/upload.ts
+++ enum/upload.ts
@@ -3,6 +3,7 @@ import { $enum } from 'ts-enum-util';
 export enum UploadType {
   PICTURE = 'PICTURE',
   AVATAR = 'AVATAR',
+  USER_COVER = 'USER_COVER',
 }
 
 export const UploadTypeValues = $enum(UploadType).map(key => UploadType[key]);
